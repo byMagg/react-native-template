d144df657acde940f8ebb3d4b56ced538d1da4f0
byMagg <byMag@hotmail.es>
2023-08-28 07:16:20 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useCallback} from 'react';
+import {Provider} from 'react-redux';
+import FlashMessage from 'react-native-flash-message';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+
+import store from './src/store/store';
+import StackNavigator from './src/navigation/StackNavigator';
+
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    'DMSans-Regular': require('./src/assets/fonts/DMSans-Regular.ttf'),
+    'DMSans-Medium': require('./src/assets/fonts/DMSans-Medium.ttf'),
+    'DMSans-Bold': require('./src/assets/fonts/DMSans-Bold.ttf'),
+    'Inter-Medium': require('./src/assets/fonts/Inter-Medium.ttf'),
+    'Inter-SemiBold': require('./src/assets/fonts/Inter-SemiBold.ttf'),
+    'Inter-Bold': require('./src/assets/fonts/Inter-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <Provider store={store}>
+        <StackNavigator />
+        <FlashMessage position="top" />
+      </Provider>
+    </SafeAreaProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
